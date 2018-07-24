2ae0cd5dd8ef0985f95a40be8566749d1f286779
Unknown <malcolmcarelse@gmail.com>
2018-07-24 11:14:37 +0200

    Added Footer Component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import "./style";
+class Footer extends Component {
+    render() {
+        return (
+        <footer>
+        <div>
+        <div style={{color:textPrimary}}>Made By Malcolm </div>
+        </div>
+            </footer>
+        );
+    }
+}
+
+export default Footer;
