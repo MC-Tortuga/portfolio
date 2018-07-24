fbad3fa0eaba9ac33d81489da5cbc7e44baa8a3d
Unknown <malcolmcarelse@gmail.com>
2018-07-24 13:19:45 +0200

    Added Nav Bar & fixed Footer index.js

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { toElement as scrollToElement } from '@utils/scroll';
+
+import './style.scss';
+
 
 class Nav extends Component {
     constructor(props) {
@@ -47,11 +52,46 @@ class Nav extends Component {
         :{backgroundColor:bgPrimary,color:colorPrimary}
 
         return (
-            <div>
-                
-            </div>
+            <nav
+        className={stickyClass}
+        ref={(elem) => {
+          this.nav = elem;
+        }}
+        style={stickyStyles}
+      >
+        <div className="magic-wand bounce-xy" onClick={(e) => switchTheme()}>
+          <button className="fas fa-magic fa-lg" href="#" />
+          <div className="magic-text">Color Me</div>
+        </div>
+        <style jsx="true">
+          {`
+            .menu__item:hover {
+              border-bottom: 2px solid ${colorPrimary};
+            }
+          `}
+        </style>
+        <div className="menu">
+          <div
+            className="menu__item active"
+            onClick={(e) => this.scrollToPage('.about-page')}
+          >
+            About
+          </div>
+          <div
+            className="menu__item"
+            onClick={(e) => this.scrollToPage('.portfolio-page')}
+          >
+            Portfolio
+          </div>
+        </div>
+      </nav>
         );
     }
 }
+
+Nav.contextTypes ={
+    theme:PropTypes.any,
+    switchTheme:PropTypes.func
+};
 
 export default Nav;
